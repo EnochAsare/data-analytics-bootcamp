6da1e9982b5ffaf77567ea5f744130034f595c1c
EnochAsare <asareenoch2004@gmail.com>
2026-08-04 18:21:41 +0000

    Add SQL tutorial for SELECT statements
    Added an unguided tutorial demonstrating various SELECT statements using the sakila database.

---
+++ sql/SQL-Practice-Unguided-.sql
@@ -0,0 +1,25 @@
+-- Unguided tutorial on SELECT STATEMENT
+select * 
+from sakila.actor;
+
+select first_name,
+last_name,
+last_update
+from sakila.actor;
+
+select first_name,
+last_name,
+last_update,
+10 + 11
+from sakila.actor;
+
+Select *
+from sakila.actor_info;
+
+select last_name,
+first_name
+from sakila.actor_info;
+
+select distinct film_info
+from sakila.actor_info;
+
